ddb6bd54d31c75a2a61605693b23b71f6ebd7bb1
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-07-29 03:13:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 //enabling cors
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://veetechedu.netlify.app",
     credentials: true,
   })
 );
