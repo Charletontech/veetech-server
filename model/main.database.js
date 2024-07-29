feb201b4c627a20584f6e3ef78d7af64002334a5
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-07-29 09:14:09 -0700

    Update main.database.js

--- model/main.database.js
+++ model/main.database.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connectDB = mysql.createConnection({
-  host: process.env.VT_HOST,
-  user: process.env.VT_USER,
-  password: process.env.VT_PASSWORD,
+  host: `${process.env.VT_HOST}`,
+  user: `${process.env.VT_USER},
+  password: `${process.env.VT_PASSWORD}`,
   port: 3307,
-  database: process.env.VT_DB,
+  database: `${process.env.VT_DB}`,
 });
 
 module.exports = connectDB;
