16ed5689d7936d6070935eea1cc65f2745479f61
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-07-29 03:18:18 -0700

    Update main.database.js

--- model/main.database.js
+++ model/main.database.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 const connectDB = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "veetech",
+  host: process.env.VT_HOST,
+  user: process.env.VT_USER,
+  password: process.env.VT_PASSWORD,
+  port: 3307,
+  database: "process.env.VT_DB",
 });
 
 module.exports = connectDB;
